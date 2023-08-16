82a87aefd323210a5e185255de421419e230d188
pinchao <cpc4253@163.com>
2023-08-16 15:24:36 +0800

    callback in loader context

--- loaders/replaceLoader.js
+++ loaders/replaceLoader.js
@@ -1,4 +1,13 @@
 module.exports = function (source) {
   // return source.replace('world', 'replace-loader');
-  return source.replace('world', this.query.name);
+  // return source.replace('world', this.query.name);
+
+  const content = source.replace('world', this.query.name);
+  // this.callback(
+  //   err: Error | null,
+  //   content: string | Buffer,
+  //   sourceMap?: SourceMap,
+  //   meta?: any
+  // );
+  return this.callback(null, content);
 };
